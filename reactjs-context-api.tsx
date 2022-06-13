bc641aeb6113f9cf041bdadb2f8523ed4e5a1d71
HU$H <54246375+hushaudio@users.noreply.github.com>
2022-06-13 09:52:14 -0500

    Update reactjs-context-api.tsx

--- reactjs-context-api.tsx
+++ reactjs-context-api.tsx
@@ -22,8 +22,10 @@ export  const GalleryContextProvider = ({ children } : any) => {
   }, [])
 
   const context = {
-    galleries,
-    getGalleries: async () => {
+    
+    galleries, // this is from our state variable created above (line 17) and can be accessed from anywhere in the app
+
+    getGalleries: async () => { // this is how we would run a mutation on the state
       // EXAMPLE METHOD
       fetch('/api/art/galleries')
         .then(r=>r.json())
@@ -32,13 +34,17 @@ export  const GalleryContextProvider = ({ children } : any) => {
         }
       )
     },
-    setGalleries: (galleries:Gallery[]) => {
+    
+    setGalleries: (galleries:Gallery[]) => { // another example of mutating the state
       // EXAMPLE METHOD
       setGalleries(galleries)
     }
+
   }
 
   return (
+    // pass the actual context functionality we built in the context variable (line 24) into the context component we created at the top (line 6)
+    // and then include the children that the context will be wrapped around.  Context api essentially wraps your whole app in the context, allowing you to access the state of it anywhere
     <GalleryContext.Provider value={context}>
       { children }
     </GalleryContext.Provider>
